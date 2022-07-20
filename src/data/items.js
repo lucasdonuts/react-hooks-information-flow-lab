fe5415b6885ce976e42d195c4038993f81d23049
Lucas Duncan <lucasdonuts@gmail.com>
2022-07-20 11:12:17 -0500

    Lab complete

--- src/data/items.js
+++ src/data/items.js
@@ -4,6 +4,11 @@ const items = [
   { id: 3, name: "Lettuce", category: "Produce" },
   { id: 4, name: "String Cheese", category: "Dairy" },
   { id: 5, name: "Cookies", category: "Dessert" },
+  { id: 6, name: "Milk", category: "Dairy" },
+  { id: 7, name: "Carrots", category: "Produce" },
+  { id: 8, name: "Brownies", category: "Dessert" },
+  { id: 9, name: "Broccoli", category: "Produce" },
+  { id: 10, name: "Ice Cream", category: "Dessert" },
 ];
 
 export default items;
